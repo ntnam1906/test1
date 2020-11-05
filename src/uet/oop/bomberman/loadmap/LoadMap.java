0be95874bcfa30e61ce1e3b8db1124be968c946b
ginsama01 <ginsama2001@gmail.com>
2020-11-05 08:08:39 +0700

    First commit

---
+++ src/uet/oop/bomberman/loadmap/LoadMap.java
@@ -0,0 +1,14 @@
+package uet.oop.bomberman.loadmap;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Scanner;
+
+public class LoadMap {
+    public static String[] loadMap(String address) {
+        try {
+            Scanner sc = new Scanner(new File("res/levels/Level1.txt"));
+
+        }
+    }
+}
